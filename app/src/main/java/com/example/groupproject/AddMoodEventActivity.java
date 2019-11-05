7ffdbd9ac6d497377bfc20a5cbf6ff5696923980
Andrewjjj <Juwon1@ualberta.ca>
2019-11-05 16:33:31 -0700

    Added some functionalities for location / google maps in our current project

--- app/src/main/java/com/example/groupproject/AddMoodEventActivity.java
+++ app/src/main/java/com/example/groupproject/AddMoodEventActivity.java
@@ -1,7 +1,12 @@
 package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -17,7 +22,10 @@ import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -28,6 +36,12 @@ public class AddMoodEventActivity extends AppCompatActivity {
     ListView moodEventList;
     ListMoodEventsAdapter moodEventAdapter;
     ArrayList<MoodEvent> moodEventDataList;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Boolean mLocationPermissionGranted;
+    static int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    // TODO: For hard code test. May be modified later.
+    Location myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +94,18 @@ public class AddMoodEventActivity extends AppCompatActivity {
 
         });
 
+        // TODO: Add some more permission safety stuff
+        getLocationPermission();
+        myLocation = getCurrentlocation();
+        System.out.println("Checking Location Permission: " + checkLocationPermission());
+        if(myLocation != null){
+            System.out.println("Lat: " + myLocation.getLatitude() + ", Lon: " + myLocation.getLongitude());
+        }
+        else{
+            System.out.println(myLocation);
+        }
+
+
         if (true) {
             moodEventDataList.add(new MoodEvent(new Happy(), new GregorianCalendar(1, 2, 3), SocialSituation.NONE, "ABC", null, null, 0));
             moodEventDataList.add(new MoodEvent(new Sad(), new GregorianCalendar(2017,06,03), SocialSituation.NONE, "ABC", null, null, 0));
@@ -88,5 +114,54 @@ public class AddMoodEventActivity extends AppCompatActivity {
             moodEventDataList.add(new MoodEvent(new Disgusted(), new GregorianCalendar(2015,01,02), SocialSituation.NONE, "ABC", null, null, 0));
         }
 
+    }
+
+    public Location getCurrentlocation(){
+        final Location[] myLocation = new Location[1];
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            System.out.println("LOCATION");
+                            // Logic to handle location object
+                            myLocation[0] = location;
+                            System.out.println("Lat: " + location.getLatitude() + ", Lon: " + location.getLongitude());
+//                            LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng,10.0f));
+//                            mMap.addMarker(new MarkerOptions().position(myLatLng).title("My Current Position"));
+//                            location.getAltitude()
+                        }
+                    }
+                });
+//        Location currentLocation = new Location("currentLocation");
+//        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+//            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
+//                    1000, mLocationListener);
+//        }
+
+        return myLocation[0];
+    }
+
+    private void getLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private boolean checkLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
