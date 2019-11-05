8afffe2340a07ea709718a163ccacace5fa50586
Vivek Acharya <rec@vach.ca>
2019-11-05 16:57:44 -0700

    user requests xml

---
+++ app/src/main/java/com/example/groupproject/ui/relations/PersonsListFragment.java
@@ -0,0 +1,13 @@
+package com.example.groupproject.ui.relations;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+// Fragment class which holds a list of people
+public class PersonsListFragment extends Fragment {
+
+//    @Override
+//    public static PersonsListFragment newInstance(@Nullable final Bundle bundle)
+}
