40b83855f644fbbbb8e5a7cbc8616484722181c5
vach0x4 <mail@vach.ca>
2019-11-08 01:36:51 -0700

    Populated Relations list.

--- app/src/main/java/com/example/groupproject/ui/relations/FollowersFragment.java
+++ app/src/main/java/com/example/groupproject/ui/relations/FollowersFragment.java
@@ -1,6 +1,18 @@
 package com.example.groupproject.ui.relations;
 
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.groupproject.R;
+
 public class FollowersFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_relations, viewGroup, false);
+    }
 }
--- app/src/main/java/com/example/groupproject/ui/relations/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/groupproject/ui/relations/SectionsPagerAdapter.java
@@ -27,21 +27,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
-//        int newPosition = position + 1;
-//        // Return a fragment based on which position the user is in
-//        switch (newPosition){
-//            case 0://TODO: serve from arry
-//                FollowingFragment.newInstance();
-//            case 1:
-//                FollowersFragment.newInstance();
-//            case 2:
-//                RequestsFragment.newInstance();
-//        }
-//        switch  (position){
-//            case 0:
-//                FollowingFragment.newInstance();
-//        }
-        return PlaceholderFragment.newInstance(position + 1);
+        return GeneralizedFragment.newInstance(position + 1);
     }
 
     @Nullable
@@ -52,7 +38,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 3;
+        // Show 3 total pages.
+        return TAB_TITLES.length;
     }
 }
