ac03755d62c01b2d9942b403db07962ef8227019
riona-wiberg <rionawiberg@hotmail.com>
2019-11-28 03:42:51 -0700

    Maps UI
    Changes to dropdown menu and formatting

--- app/src/main/java/com/example/groupproject/ui/maps/MapsSpinnerAdapter.java
+++ app/src/main/java/com/example/groupproject/ui/maps/MapsSpinnerAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.groupproject.R;
@@ -15,6 +16,7 @@ public class MapsSpinnerAdapter extends ArrayAdapter {
 
     Integer[] images = {0, R.drawable.emot_happy_small, R.drawable.emot_sad_small, R.drawable.emot_angry_small, R.drawable.emot_anxious_small, R.drawable.emot_disgusted_small};
     String[] moodNames = {"Show All", "Happy", "Sad", "Angry", "Anxious", "Disgusted"};
+    Integer[] colors = {0xfff0f0f0, 0x5bffff00, 0x5b0090ff, 0x5bff0000, 0x5bC997ff, 0x5b00ff00};
 
     public MapsSpinnerAdapter(Context context, int textViewResourceId, String[] names){
         super(context, textViewResourceId, names);
@@ -24,11 +26,17 @@ public class MapsSpinnerAdapter extends ArrayAdapter {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View layout = inflater.inflate(R.layout.activity_maps_spinner, parent, false);
 
+        LinearLayout spinnerElement = layout.findViewById(R.id.spinnerElement);
+        spinnerElement.setBackgroundColor(colors[position]);
+
         ImageView imageView = layout.findViewById(R.id.mapSpinnerImage);
         imageView.setImageResource(images[position]);
 
+
         TextView textView = layout.findViewById(R.id.mapSpinnerText);
         textView.setText(moodNames[position]);
+
+
 
         return layout;
     }
